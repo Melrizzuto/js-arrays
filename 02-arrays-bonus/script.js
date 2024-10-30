1030cb04a1a3a7b2091657c6c2e3a41cb02f5785
Melrizzuto <melrizzuto4@gmail.com>
2024-10-30 16:35:57 +0100

    changed ex 2 bonus  js

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,7 +15,30 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter(teacher => teacher.length >= 5);
+// arrow function
+// const longNames = teachers.filter(teacher => teacher.length >= 5);
+// funzione anonima
+/* const longNames = teachers.filter(function(teacher) { 
+  if (teacher.length >= 5) {
+    return teacher;
+  }
+}); */
+// funzione classica
+// const longNames = teachers.filter(teacherLenght);
+// function teacherLenght(teacher) { 
+//   if (teacher.length >= 5) {
+//     return teacher;
+//   }
+// }
+
+
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
